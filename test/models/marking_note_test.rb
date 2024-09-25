62487f19edb7b60197a8422df2b4d833d6bec845
bodand <bodand@proton.me>
2024-09-25 17:53:02 +0200

    test: marking note base tests

--- test/models/marking_note_test.rb
+++ test/models/marking_note_test.rb
@@ -1,7 +1,13 @@
 require "test_helper"
 
 class MarkingNoteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "points cost equals effective cost if note is not fixed" do
+    n = MarkingNote.new(points_cost: 42, fixed: false)
+    assert_equal n.points_cost, n.effective_cost
+  end
+
+  test "effective cost is 0 if note is fixed" do
+    n = MarkingNote.new(points_cost: 42, fixed: true)
+    assert_equal 0, n.effective_cost
+  end
 end
