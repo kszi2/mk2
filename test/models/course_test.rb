e2a6c99ecd5fa07931061b1cc2b4cf483b29df36
bodand <bodand@proton.me>
2024-09-25 16:55:47 +0200

    test: course model
    more tests for the model course

--- test/models/course_test.rb
+++ test/models/course_test.rb
@@ -9,20 +9,48 @@ class CourseTest < ActiveSupport::TestCase
     assert c.valid?
   end
 
+  test "course without name is invalid" do
+    c = make_course name: nil
+    refute c.valid?
+  end
+
+  test "course without name reports missing name" do
+    c = make_course name: nil
+    refute c.valid?
+    assert_match /can't be blank/, c.errors[:name].inject(:+)
+  end
+
   test "course with too short name is invalid" do
     c = make_course(name: "a")
     refute c.valid?
   end
 
-  test "course with too short name contains proper error" do
+  test "course with too short name reports this" do
     c = make_course(name: "a")
-    c.validate
+    refute c.valid?
     assert_match /is too short/, c.errors[:name].inject(:+)
   end
 
-  test "course with duplicate name is invalid" do
-    c = make_course(name: "Course 1")
+  test "course with too long name is invalid" do
+    c = make_course(name: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
     refute c.valid?
+  end
+
+  test "course with too long name reports this" do
+    c = make_course(name: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+    refute c.valid?
+    assert_match /is too long/, c.errors[:name].inject(:+)
+  end
+
+  test "course with duplicate name is invalid" do
+    c = make_course(name: courses(:course_with_lab).name)
+    refute c.valid?
+  end
+
+  test "course with duplicate name contains reports violation" do
+    c = make_course(name: courses(:course_with_lab).name)
+    refute c.valid?
+    assert_match /already been taken/, c.errors[:name].inject(:+)
   end
 
   private
