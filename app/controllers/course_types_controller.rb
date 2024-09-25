2f107eca49f94ffe4f5cbbc9916f5b46fd5e92bc
bodand <bodand@proton.me>
2024-09-25 18:49:46 +0200

    test: coures_types pages tests

--- app/controllers/course_types_controller.rb
+++ app/controllers/course_types_controller.rb
@@ -40,7 +40,7 @@ class CourseTypesController < ApplicationController
   def update
     respond_to do |format|
       if @course_type.update(course_type_params)
-        format.html { redirect_to course_type_url(@course_type), notice: "Course type was successfully updated." }
+        format.html { redirect_to course_course_type_path(@course, @course_type), notice: "Course type was successfully updated." }
         format.json { render :show, status: :ok, location: @course_type }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -54,7 +54,7 @@ class CourseTypesController < ApplicationController
     @course_type.destroy!
 
     respond_to do |format|
-      format.html { redirect_to course_types_url, notice: "Course type was successfully destroyed." }
+      format.html { redirect_to course_path(@course, page: :course_type), notice: "Course type was successfully destroyed." }
       format.json { head :no_content }
     end
   end
--- test/controllers/course_types_controller_test.rb
+++ test/controllers/course_types_controller_test.rb
@@ -2,47 +2,102 @@ require "test_helper"
 
 class CourseTypesControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @course_type = course_types(:one)
+    @course = courses(:course_with_lab)
+    @course_type = course_types(:labor_1)
   end
 
-  test "should get index" do
-    get course_types_url
+  test "index exists" do
+    get course_course_types_url(@course)
     assert_response :success
+  end
+
+  test "index contains all types for course once" do
+    get course_course_types_url(@course)
+    assert_response :success
+    @course.course_types.each do |type|
+      assert_dom "td", { text: type.name, count: 1 }
+    end
   end
 
   test "should get new" do
-    get new_course_type_url
+    get new_course_course_type_url @course
     assert_response :success
+  end
+
+  test "new contains input field for name" do
+    get new_course_course_type_url @course
+    assert_response :success
+    assert_dom "wa-input[name='course_type[name]']", { count: 1 }
+  end
+
+  test "new contains label for name" do
+    get new_course_course_type_url @course
+    assert_response :success
+    assert_dom "wa-input[label='Name']", { count: 1 }
   end
 
   test "should create course_type" do
-    assert_difference("CourseType.count") do
-      post course_types_url, params: { course_type: { course_id: @course_type.course_id, name: @course_type.name } }
+    @course_type = CourseType.new(name: "controller-test-course-type", course: @course)
+    assert_difference -> { CourseType.count } do
+      post course_course_types_url(@course), params: {
+        course_type: { course_id: @course_type.course_id, name: @course_type.name }
+      }
     end
 
-    assert_redirected_to course_type_url(CourseType.last)
+    assert_redirected_to course_course_type_url(@course, CourseType.last)
   end
 
   test "should show course_type" do
-    get course_type_url(@course_type)
+    get course_course_type_url(@course, @course_type)
     assert_response :success
+  end
+
+  test "show of course_type includes course type name" do
+    get course_course_type_url(@course, @course_type)
+    assert_response :success
+    assert_dom "b", { count: 1, text: "Name:" }
+    assert_dom "span", { count: 1, text: @course_type.name }
+  end
+
+  test "show of course_type includes containing course name" do
+    get course_course_type_url(@course, @course_type)
+    assert_response :success
+    assert_dom "b", { count: 1, text: "Course:" }
+    assert_dom "span", { count: 1, text: @course.name }
   end
 
   test "should get edit" do
-    get edit_course_type_url(@course_type)
+    get edit_course_course_type_url(@course, @course_type)
     assert_response :success
   end
 
+  test "edit contains input field for name" do
+    get edit_course_course_type_url @course, @course_type
+    assert_response :success
+    assert_dom "wa-input[name='course_type[name]']", { count: 1 }
+  end
+
+  test "edit contains label for name" do
+    get edit_course_course_type_url @course, @course_type
+    assert_response :success
+    assert_dom "wa-input[label='Name']", { count: 1 }
+  end
+
   test "should update course_type" do
-    patch course_type_url(@course_type), params: { course_type: { course_id: @course_type.course_id, name: @course_type.name } }
-    assert_redirected_to course_type_url(@course_type)
+    assert_changes -> { @course_type.name }, from: @course_type.name, to: "new name" do
+      patch course_course_type_url(@course, @course_type), params: {
+        course_type: { course_id: @course_type.course_id, name: "new name" }
+      }
+      @course_type.reload
+    end
+    assert_redirected_to course_course_type_url(@course, @course_type)
   end
 
   test "should destroy course_type" do
     assert_difference("CourseType.count", -1) do
-      delete course_type_url(@course_type)
+      delete course_course_type_url(@course, @course_type)
     end
 
-    assert_redirected_to course_types_url
+    assert_redirected_to course_url(@course, page: :course_type)
   end
 end
