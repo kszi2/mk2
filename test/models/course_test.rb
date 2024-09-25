6325284004aa07bc21ead8ff79359cbe32ef06b5
bodand <bodand@proton.me>
2024-09-25 16:48:51 +0200

    test: course model tests
    added tests for model course

--- test/models/course_test.rb
+++ test/models/course_test.rb
@@ -4,4 +4,30 @@ class CourseTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "course with correct length and unique name is valid" do
+    c = make_course
+    assert c.valid?
+  end
+
+  test "course with too short name is invalid" do
+    c = make_course(name: "a")
+    refute c.valid?
+  end
+
+  test "course with too short name contains proper error" do
+    c = make_course(name: "a")
+    c.validate
+    assert_match /is too short/, c.errors[:name].inject(:+)
+  end
+
+  test "course with duplicate name is invalid" do
+    c = make_course(name: "Course 1")
+    refute c.valid?
+  end
+
+  private
+
+  def make_course(**kwargs)
+    Course.new(name: 'test-course', **kwargs)
+  end
 end
