473cdbf437d6109f8d93948e1d55aaca13c898a4
bodand <bodand@proton.me>
2024-09-25 18:12:12 +0200

    test: pdf_datum basic tests

--- test/models/pdf_datum_test.rb
+++ test/models/pdf_datum_test.rb
@@ -1,7 +1,23 @@
 require "test_helper"
 
 class PdfDatumTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "pdf-datum stores name for file" do
+    datum = PdfDatum.from_raw(empty_pdf, "fun.pdf")
+    assert_equal "fun.pdf", datum.name
+  end
+
+  test "pdf-datum saves the data passed to it to a file" do
+    pdf = empty_pdf
+    datum = PdfDatum.from_raw(pdf, "fun.pdf")
+    assert_equal pdf, datum.file.download
+  end
+
+  private
+
+  def empty_pdf
+    pdf = Prawn::Document.new do
+      text "AAAA"
+    end
+    pdf.render
+  end
 end
