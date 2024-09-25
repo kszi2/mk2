349ea94d25fab2a1f781dd81434fa844ddb0e746
bodand <bodand@proton.me>
2024-09-25 20:08:01 +0200

    test: course types nav buttons

--- test/controllers/course_types_controller_test.rb
+++ test/controllers/course_types_controller_test.rb
@@ -66,6 +66,19 @@ class CourseTypesControllerTest < ActionDispatch::IntegrationTest
     assert_dom "span", { count: 1, text: @course.name }
   end
 
+  test "show has edit button to edit page of current course" do
+    get course_course_type_url(@course, @course_type)
+    assert_response :success
+    assert_dom "wa-button[href='#{edit_course_course_type_path(@course, @course_type)}']", { count: 1, text: "Edit" }
+  end
+
+  test "show has back button to courses index" do
+    get course_course_type_url(@course, @course_type)
+    assert_response :success
+    assert_dom "wa-button[href='#{course_path(@course, page: :course_types)}']", { count: 1, text: "Back" }
+  end
+
+
   test "should get edit" do
     get edit_course_course_type_url(@course, @course_type)
     assert_response :success
@@ -83,6 +96,24 @@ class CourseTypesControllerTest < ActionDispatch::IntegrationTest
     assert_dom "wa-input[label='Name']", { count: 1 }
   end
 
+  test "edit has show button to page of current course" do
+    get edit_course_course_type_url(@course, @course_type)
+    assert_response :success
+    assert_dom "wa-button[href='#{course_course_type_path(@course, @course_type)}']", { count: 1, text: "Show" }
+  end
+
+  test "edit has back button to courses index" do
+    get edit_course_course_type_url(@course, @course_type)
+    assert_response :success
+    assert_dom "wa-button[href='#{course_path(@course, page: :course_types)}']", { count: 1, text: "Back" }
+  end
+
+  test "edit has save button" do
+    get edit_course_course_type_url(@course, @course_type)
+    assert_response :success
+    assert_dom "wa-button[type='submit']", { count: 1, text: "Save" }
+  end
+
   test "should update course_type" do
     assert_changes -> { @course_type.name }, from: @course_type.name, to: "new name" do
       patch course_course_type_url(@course, @course_type), params: {
@@ -98,6 +129,6 @@ class CourseTypesControllerTest < ActionDispatch::IntegrationTest
       delete course_course_type_url(@course, @course_type)
     end
 
-    assert_redirected_to course_url(@course, page: :course_type)
+    assert_redirected_to course_url(@course, page: :course_types)
   end
 end
