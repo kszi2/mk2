6fff3028584335f990f3dcf8a664b5f5829e60c2
bodand <bodand@proton.me>
2025-02-12 15:12:34 +0100

    test: sizable

---
+++ test/components/sizeable_test.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+require 'minitest/autorun'
+
+class SizeableTest < ActiveSupport::TestCase
+  class Sut
+    include Sizable
+
+    def initialize(size) = self.size = size
+  end
+
+  test "size can be read" do
+    sut = Sut.new(:medium)
+    assert_equal :medium, sut.size
+  end
+
+  test "size can be modified" do
+    sut = Sut.new(:medium)
+    sut.size = :large
+    assert_equal :large, sut.size
+  end
+
+  %i[small normal medium large].each do |size|
+    test "size_style returns non-empty string for valid size #{size}" do
+      sut = Sut.new(size)
+      refute_empty sut.size_styles
+    end
+  end
+
+  test "size_style returns same for normal and medium sizes" do
+    normal = Sut.new(:normal)
+    medium = Sut.new(:medium)
+    assert_equal normal.size_styles, medium.size_styles
+  end
+
+  test "size_style raises for invalid size" do
+    assert_raises ArgumentError, /my_size_value/ do
+      Sut.new(:my_size_value).size_styles
+    end
+  end
+end
