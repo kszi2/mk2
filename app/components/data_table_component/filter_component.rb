d4de51c5a270e959b603e39175289d9e1b080fbe
bodand <bodand@proton.me>
2025-02-12 15:52:27 +0100

    test: filter component

--- app/components/data_table_component/filter_component.rb
+++ app/components/data_table_component/filter_component.rb
@@ -5,15 +5,18 @@ class DataTableComponent::FilterComponent < ViewComponent::Base
   include Inputs
   include ComponentHelper
 
-  def initialize(controller:, field:, name: nil, current_filters: {})
+  def initialize(controller:,
+                 field:,
+                 clear_url:,
+                 filter_url:,
+                 name: nil,
+                 current_filters: {})
     @controller = controller
     @field = field
+    @clear_url = clear_url
+    @filter_url = filter_url
     @name = name || field.to_s.humanize
     @current = current_filters
-  end
-
-  def before_render
-    @clear_url = url_for(controller: @controller, action: :index)
   end
 
   def current_filter
---
+++ test/components/data_table_component/filter_component_test.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+class DataTableComponent::FilterComponentTest < ViewComponent::TestCase
+  Sut = DataTableComponent::FilterComponent
+  Controller = "submissions"
+
+  test "does render" do
+    render_inline Sut.new(controller: Controller, field: :field, clear_url: "/clear", filter_url: "/filter")
+    assert_component_rendered
+  end
+
+  test "renders form with hidden value for field for given controller and field" do
+    render_inline Sut.new(controller: Controller, field: :student, clear_url: "/clear", filter_url: "/filter")
+    assert_component_rendered
+
+    assert_css "form##{Controller}_student_form > input[type='hidden'][name='filter_field'][value='student']",
+               visible: false
+  end
+
+  test "without name filter is named humanized field" do
+    render_inline Sut.new(controller: Controller, field: :student, clear_url: "/clear", filter_url: "/filter")
+    assert_component_rendered
+
+    assert_text "Filter Student"
+  end
+
+  test "with name filter is named the given name" do
+    render_inline Sut.new(controller: Controller, field: :student, name: 'asdasdasd', clear_url: "/clear", filter_url: "/filter")
+    assert_component_rendered
+
+    assert_text "Filter asdasdasd"
+  end
+
+  test "renders button to clear filter" do
+    render_inline Sut.new(controller: Controller, field: :student, clear_url: "/clear", filter_url: "/filter")
+    assert_component_rendered
+
+    assert_link "Clear", visible: false
+  end
+
+  test "renders button to apply filter" do
+    render_inline Sut.new(controller: Controller, field: :student, clear_url: "/clear", filter_url: "/filter")
+    assert_component_rendered
+
+    assert_link "Filter", visible: false
+  end
+end
