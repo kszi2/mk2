2a52ace18332f2e55516dcd5590e114b3cdf1d92
bodand <bodand@proton.me>
2025-04-29 22:45:53 +0200

    fix: prevent empty attendance PDFs from being sent

--- app/jobs/weekly_send_needed_attendances_job.rb
+++ app/jobs/weekly_send_needed_attendances_job.rb
@@ -8,12 +8,14 @@ class WeeklySendNeededAttendancesJob < ApplicationJob
     real_semester = today.semester_range
 
     first_iteration = true
+    any_found = false
     pdf = Prawn::Document::new(page_size: 'A4') do |pdf|
       Group.includes(:course, :students)
            .where(first_date: real_semester)
            .find_each(batch_size: 100) do |group|
         # skip group, unless next week has a class
         next unless group.next_class_date.in?(today..next_week)
+        any_found = true
 
         pdf.start_new_page unless first_iteration
         group.render_attendance_sheet pdf, :next
@@ -21,6 +23,7 @@ class WeeklySendNeededAttendancesJob < ApplicationJob
       end
     end
 
+    return unless any_found
     PdfDatum.from_raw(pdf.render, "week_of_#{today.strftime("%F")}.pdf").send_to_discord
   end
 end
