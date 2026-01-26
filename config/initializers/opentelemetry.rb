fcdf11e00a2cd937f5bbdd92bf298f73c69fa221
bodand <bodand@proton.me>
2026-01-26 22:35:43 +0100

    chore: remove OpenTelemetry instrumentation and dependencies

--- config/initializers/opentelemetry.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require "opentelemetry/sdk"
-require "opentelemetry/instrumentation/all"
-
-OpenTelemetry::SDK.configure do |c|
-  c.service_name = 'mk2'
-  c.use_all # enables all instrumentation!
-end
