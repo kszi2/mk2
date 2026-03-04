5204becc270a6d62dd45452bedf7701242c05957
bodand <bodand@proton.me>
2026-03-04 20:55:23 +0100

    feat: policies v0

---
+++ app/policies/course_policy.rb
@@ -0,0 +1,22 @@
+class CoursePolicy < ApplicationPolicy
+  class Scope < ApplicationPolicy::Scope
+    # NOTE: Be explicit about which records you allow access to!
+    def resolve
+      scope.all
+    end
+  end
+
+  def index? = true
+
+  def show? = true
+
+  def new? = true
+
+  def create? = true
+
+  def edit? = true
+
+  def update? = true
+
+  def destroy? = true
+end
---
+++ app/policies/course_type_policy.rb
@@ -0,0 +1,22 @@
+class CourseTypePolicy < ApplicationPolicy
+  class Scope < ApplicationPolicy::Scope
+    # NOTE: Be explicit about which records you allow access to!
+    def resolve
+      scope.all
+    end
+  end
+
+  def index? = true
+
+  def show? = true
+
+  def new? = true
+
+  def create? = true
+
+  def edit? = true
+
+  def update? = true
+
+  def destroy? = true
+end
